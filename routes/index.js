a26a5071133b356ebc2af87744777f8aa0cf4e1b
David Lopez <letsbelopez@gmail.com>
2020-04-28 17:25:05 -0700

    generate routes based on static files

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,41 @@
 const express = require('express');
+const path = require('path');
 const fs = require('fs');
-const showdown = require('showdown');
 
 const router = express.Router();
-const mkdwnConverter = new showdown.Converter();
+const contentDir = path.join(__dirname, '../content');
 
-router.get('/', function (req, res) {
-  const post = fs.readFileSync(`${__dirname}/../posts/first.md`, 'utf8');
-
-  res.render('index', { post: mkdwnConverter.makeHtml(post) });
+fs.readdir(path.join(__dirname, '../content'), function(err, files) {
+  if (err) return console.error("Couldn't start the server");
+  const data = getContentData(contentDir, files);
+  createRoutes(data);
 });
+
+function getContentData(basePath, files, result = []) {
+  for (let i = 0; i < files.length; i++) {
+    const current = files[i];
+    let newbase = path.join(basePath, current);
+    if (fs.statSync(newbase).isDirectory()) {
+      getContentData(newbase, fs.readdirSync(newbase), result);
+    } else {
+      const route = {
+        date: current.slice(0, 10),
+        slug: current.slice(11).replace('.html', ''),
+        path: newbase
+      }
+      result.push(route);
+    }
+  }
+  return result;
+}
+
+function createRoutes(data) {
+  data.forEach(d => {
+    router.get(`/${d.slug}`, function(req, res) {
+      let content = fs.readFileSync(d.path, 'utf8');
+      res.render('index', { content });
+    });
+  });
+}
 
 module.exports = router;
