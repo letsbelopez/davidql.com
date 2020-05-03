7f7edbd63bd51b69812b54d73d5b4e8d15cc0028
David Lopez <letsbelopez@gmail.com>
2020-05-03 09:48:53 -0700

    read posts from plain text

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
+const readline = require('readline');
 const fs = require('fs');
 
 const router = express.Router();
 const contentDir = path.join(__dirname, '../content');
 
-fs.readdir(path.join(__dirname, '../content'), function(err, files) {
+fs.readdir(path.join(__dirname, '../content'), async (err, files) => {
   if (err) return console.error("Couldn't start the server");
-  const data = getContentData(contentDir, files);
+  const data = await getContentData(contentDir, files);
   createRoutes(data);
 });
 
@@ -18,11 +19,15 @@ function getContentData(basePath, files, result = []) {
     if (fs.statSync(newbase).isDirectory()) {
       getContentData(newbase, fs.readdirSync(newbase), result);
     } else {
+      const lines = getLines(newbase);
+      const title = lines[0];
+      const body = lines.slice(1).join('');
       const route = {
         date: current.slice(0, 10),
-        slug: current.slice(11).replace('.html', ''),
-        path: newbase
-      }
+        slug: current.slice(11),
+        title,
+        body,
+      };
       result.push(route);
     }
   }
@@ -30,12 +35,21 @@ function getContentData(basePath, files, result = []) {
 }
 
 function createRoutes(data) {
-  data.forEach(d => {
-    router.get(`/${d.slug}`, function(req, res) {
-      let content = fs.readFileSync(d.path, 'utf8');
-      res.render('index', { content });
+  data.forEach((d) => {
+    router.get(`/${d.slug}`, function (req, res) {
+      res.render('blog', { date: d.date, title: d.title, body: d.body });
     });
   });
 }
+
+function getLines(path) {
+  var data = fs.readFileSync(path, 'utf8');
+  return data.split('\n');
+}
+
+// Home page route
+router.get(`/`, function (req, res) {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
 
 module.exports = router;
