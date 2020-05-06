551e663a2d638d3fa5906d7fb0ced5672b18b7f9
David Lopez <letsbelopez@gmail.com>
2020-05-05 20:36:51 -0700

    port var for cloud deploy

--- start.js
+++ start.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-app.set("port", 7777);
+app.set("port", process.env.PORT || 7777);
 
 const server = app.listen(app.get("port"), () => {
   console.log(`Express running - PORT ${server.address().port}`);
