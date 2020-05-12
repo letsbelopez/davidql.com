a9c622e8f702dfb98b6bb77ab858ebab65414a28
Alejandra Lopez <Alej@Alejandras-MacBook-Air.local>
2020-05-12 16:54:55 -0700

    Update to use sendFile instead of express.static

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const path = require('path');
+const config = require('./config');
 
 // Create the express app
 const app = express();
 
-// serves up static files from the public folder
-app.use(express.static(path.join(__dirname, 'public'), { extensions: ['html'] }));
-app.use(express.static(path.join(__dirname, 'public/assets')));
+// serves up static assets
+app.use(express.static(path.join(__dirname, 'assets')));
+
+// All other content routes
+app.use('/:slug', function(req, res) {
+  res.sendFile(`${config.outdir}/${req.params.slug}.html`);
+});
+
+// Home
+app.use('/', function(req, res) {
+  res.sendFile(`${config.outdir}/index.html`);
+});
 
 module.exports = app;
