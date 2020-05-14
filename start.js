92ab37d3292b7151c1f8186b4f31dfc43c869542
Alejandra Lopez <Alej@Alejandras-MacBook-Air.local>
2020-05-13 21:08:51 -0700

    remove gcloud stuff

--- start.js
+++ start.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-app.set("port", process.env.PORT || 7777);
+app.set("port", 3000);
 
 const server = app.listen(app.get("port"), () => {
   console.log(`Express running - PORT ${server.address().port}`);
