868574516e39692cb995eca235f1b4a71eea0dc0
Alejandra Lopez <Alej@Alejandras-MacBook-Air.local>
2020-05-13 21:30:27 -0700

    create outdirectory if doesn't exist.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const contentController = require('./content');
 
 const files = fs.readdirSync(config.contentdir);
 const data = contentController.getContentData(config.contentdir, files);
+
+if (!fs.existsSync(config.outdir)) {
+  fs.mkdirSync(config.outdir);
+}
+
 contentController.createPages(data);
 contentController.createHomePage();
 
