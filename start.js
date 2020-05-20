5622ecafe244980a2122b4548917022b7d1127b0
David Lopez <dlo@Davids-MacBook-Pro.local>
2020-05-19 21:31:36 -0700

    finish fsj

--- start.js
+++ start.js
@@ -1,6 +1,11 @@
 const app = require("./app");
 app.set("port", 3000);
 
+if (process.env.NODE_ENV === 'dev') {
+  // build the code
+  require('./src/index');
+}
+
 const server = app.listen(app.get("port"), () => {
   console.log(`Express running - PORT ${server.address().port}`);
 });
