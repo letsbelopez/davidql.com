faa109938518ee3c999c325defbfdfbc3d776c0d
David Lopez <dlo@davidlopez.dev>
2021-02-18 11:59:21 -0800

    deleting old files before adding nextjs project

--- config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-const config = {
-    contentdir: path.join(__dirname, './content'),
-    outdir: path.join(__dirname, './public'),
-    viewsdir: path.join(__dirname, './views')
-}
-
-module.exports = config;
